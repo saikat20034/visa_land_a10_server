5d0a70ab8600977137627296bbf5fbec0599545e
saikat20034 <mazharulislamsaikat2000@gmail.com>
2024-12-08 12:56:18 +0600

    Cancel from my visa application

--- index.js
+++ index.js
@@ -110,10 +110,10 @@ async function run() {
 
 
 
-    await client.db('admin').command({ ping: 1 });
-    console.log(
-      'Pinged your deployment. You successfully connected to MongoDB!'
-    );
+    // await client.db('admin').command({ ping: 1 });
+    // console.log(
+    //   'Pinged your deployment. You successfully connected to MongoDB!'
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -121,6 +121,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-// app.listen(port, () => {
-//   console.log(`Visa Land server listening on port ${port}`);
-// });
+app.listen(port, () => {
+  console.log(`Visa Land server listening on port ${port}`);
+});
